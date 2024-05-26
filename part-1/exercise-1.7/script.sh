69c360d23a02bbc8cc75f4b4eb6d86f8f2bd1365
nikza <156214902+nikzaim@users.noreply.github.com>
2024-05-26 23:53:13 +0800

    Add Dockerfile for yt-dlp

--- part-1/exercise-1.7/script.sh
+++ part-1/exercise-1.7/script.sh
@@ -1,6 +1,14 @@
+# while true
+# do
+#   echo "Input website:"
+#   read website; echo "Searching.."
+#   sleep 1; curl http://$website
+# done
+
+#!/bin/bash
 while true
 do
-  echo "Input website:"
-  read website; echo "Searching.."
-  sleep 1; curl http://$website
+echo "Searching..";
+sleep 1;
+curl http://$1;
 done
